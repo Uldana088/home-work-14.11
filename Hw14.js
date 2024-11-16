f0d2c2360f1242202cb2de2302c71074cc329abc
Abdiomar Uldana <Abdiumar@list.ru>
2024-11-16 20:30:44 +0500

    home work

---
+++ Hw14.js
@@ -0,0 +1,18 @@
+let password = "amjilt_the_best";
+let password2 = 5;
+
+// Цикл арқылы пароль сұрау
+for (let attempt = 0; attempt < password2; attempt++) {
+    let password3 = prompt("Парольді енгізіңіз:");
+
+    if (password3 === password) {
+        alert("пароль дурыс");
+        break;
+    } else {
+        alert(` ${password2 - attempt - 1} мүмкіндік қалды`);
+    }
+
+    if (attempt === password2 - 1) {
+        alert("Too many attempts");
+    }
+}
